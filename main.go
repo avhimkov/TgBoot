6c5cc63909b01cea5d7b4adf1134b70359792d65
Dmitry <avhimkov@mail.ru>
2019-04-16 22:47:42 +0500

    add more examples

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
@@ -92,4 +97,124 @@ func main() {
 
 		// bot.Send(msg)
 	}
+}
+
+func timerHandler(m *tgbotapi.Message) {
+	// m.Vars contains all variables, parsed during routing
+	secondsStr := m.Vars["seconds"]
+	// Convert string variable to integer seconds value
+	seconds, err := strconv.Atoi(secondsStr)
+	if err != nil {
+		m.Reply("Invalid number of seconds")
+		return
+	}
+	m.Replyf("Timer for %d seconds started", seconds)
+	time.Sleep(time.Duration(seconds) * time.Second)
+	m.Reply("Time out!")
+}
+
+func textHandler(m *tgbotapi.Message) {
+	text := fmt.Sprintf(
+		"*%s*\n"+
+			"*Level* _%v_\n"+
+			"*School* _%s_\n"+
+			"*Time* _%s_\n"+
+			"*Range* _%s_\n"+
+			"*Components* _%s_\n"+
+			"*Duration* _%s_\n"+
+			"*Classes* _%s_\n"+
+			"*Roll* _%s_\n"+
+			"%s",
+		spell.Name,
+		spell.Level,
+		spell.School,
+		spell.Time,
+		spell.Range,
+		spell.Components,
+		spell.Duration,
+		spell.Classes,
+		strings.Join(spell.Rolls, ", "),
+		strings.Join(spell.Texts, "\n"))
+
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
+	msg.ParseMode = "markdown"
+	bot.Send(msg)
+}
+
+func buttonHandelr(m *tgbotapi.Message) {
+	switch command {
+	case "setclass":
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Select your class")
+
+		keyboard := tgbotapi.InlineKeyboardMarkup{}
+		for _, class := range classes {
+			var row []tgbotapi.InlineKeyboardButton
+			btn := tgbotapi.NewInlineKeyboardButtonData(class, class)
+			row = append(row, btn)
+			keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, row)
+		}
+
+		msg.ReplyMarkup = keyboard
+		bot.Send(msg)
+	}
+}
+
+var classesMap map[int]string
+
+type Spells struct {
+	XMLName xml.Name `xml:"compendium"`
+	Spells  []Spell  `xml:"spell"`
+}
+
+type Spell struct {
+	XMLName    xml.Name `xml:"spell"`
+	Name       string   `xml:"name"`
+	Level      int      `xml:"level"`
+	School     string   `xml:"school"`
+	Time       string   `xml:"time"`
+	Range      string   `xml:"range"`
+	Components string   `xml:"components"`
+	Duration   string   `xml:"duration"`
+	Classes    string   `xml:"classes"`
+	Texts      []string `xml:"text"`
+	Rolls      []string `xml:"roll"`
+}
+
+func parseSpells() (Spells, error) {
+	file, err := os.Open("phb.xml")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fi, err := file.Stat()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var data = make([]byte, fi.Size())
+	_, err = file.Read(data)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var v Spells
+	err = xml.Unmarshal(data, &v)
+
+	if err != nil {
+		log.Println(err)
+		return v, err
+	} else {
+		log.Printf("Total spells found: %v", len(v.Spells))
+		return v, err
+	}
+}
+
+func Filter(spells []Spell, fn func(spell Spell) bool) []Spell {
+	var filtered []Spell
+	for _, spell := range spells {
+		if fn(spell) {
+			filtered = append(filtered, spell)
+		}
+	}
+	return filtered
 }
